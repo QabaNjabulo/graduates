dfa0d6179e3e2b8e7c740b7910d70a259495db64
QabaNjabulo <u19062665@tuks.co.za>
2022-04-17 23:15:38 +0200

    Merge branch 'develop' of https://github.com/COS-301/graduates into develop

--- libs/client/notifications/feature/src/lib/client-notifications-feature.module.ts
+++ libs/client/notifications/feature/src/lib/client-notifications-feature.module.ts
@@ -1,13 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-||||||| 569687cf
-import { FooterComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/footer/footer.component';
-import { HeaderComponent } from '../../../../shared/components/header-and-footer/ui/header-and-footer/src/app/header/header.component';
-=======
->>>>>>> 006507d2d4dd612e235c3437e6ca32dc143e57bf
 import { NotifDisplayComponent } from './notif-display/notif-display.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -35,17 +27,11 @@ import { FooterModule } from '../../../../shared/components/footer/src/lib/foote
     MatListModule,
     FooterModule,
     HeaderModule,
-    HttpClientModule
-
->>>>>>> 006507d2d4dd612e235c3437e6ca32dc143e57bf
-  ],
-  declarations: [
     NotifDisplayComponent,   
     ExpansionNotifComponent
   ],
   exports: [
     NotifDisplayComponent,
-    ExpansionNotifComponent
   ],
 })
 export class ClientNotificationsFeatureModule {}
